f7a39f756bbd60985947cf0fefe077b387fb85aa
kenjimaeda65 <ricardo.maeda@inmetrics.com.br>
2023-09-08 20:04:17 -0300

    feat: finished app

--- app/src/main/java/com/example/moviesapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/moviesapp/screens/details/DetailsScreen.kt
@@ -8,11 +8,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -22,11 +31,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.moviesapp.mock.listMovies
+import com.example.moviesapp.widgtes.RowMovies
 
 //preview referencia
 //https://developer.android.com/jetpack/compose/tooling/previews
@@ -36,21 +49,25 @@ import androidx.navigation.NavController
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailsScreen(navController: NavController, movie: String?) {
-    if (movie != null) {
+fun DetailsScreen(navController: NavController, movieId: String?) {
+    if (movieId != null) {
+        val movie = listMovies.first { it.id == movieId }
+
         Scaffold(topBar = {
             TopAppBar(title = {
                 Row(
                     horizontalArrangement = Arrangement.Start,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Icon(modifier = Modifier
-                        .size(15.dp)
-                        .clickable {
-                            navController.popBackStack()
-                        },
+                    Icon(
+                        modifier = Modifier
+                            .size(15.dp)
+                            .clickable {
+                                navController.popBackStack()
+                            },
                         imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Back to pop")
+                        contentDescription = "Back to pop"
+                    )
                     Spacer(modifier = Modifier.width(100.dp))
                     Text(text = "Movies App")
                 }
@@ -61,15 +78,45 @@ fun DetailsScreen(navController: NavController, movie: String?) {
                     .fillMaxWidth()
                     .fillMaxHeight()
             ) {
+                //proprio column serve como scroll , so colocar a propriedad verticalScroll
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                    verticalArrangement = Arrangement.Top,
+                    modifier = Modifier
+                        .padding(top = it.calculateTopPadding())
+                        .verticalScroll(
+                            rememberScrollState()
+                        )
+
                 ) {
+                    RowMovies(movie = movie, handleClickCard = {})
+                    Divider(modifier = Modifier.padding(10.dp))
                     Text(
-                        text = movie, style = androidx.compose.ui.text.TextStyle(
+                        text = "Movie Images", style = androidx.compose.ui.text.TextStyle(
                             fontSize = 20.sp
                         )
                     )
+                    LazyRow {
+                        items(movie.images) {
+                            Card(
+                                modifier = Modifier
+                                    .size(200.dp)
+                                    .padding(horizontal = 15.dp, vertical = 20.dp),
+                                elevation = CardDefaults.cardElevation(
+                                    defaultElevation = 5.dp
+                                )
+                            ) {
+                                AsyncImage(
+                                    model = ImageRequest.Builder(LocalContext.current).data(it)
+                                        .build(),
+                                    contentDescription = "Image movies",
+                                    contentScale = ContentScale.FillHeight
+                                )
+                            }
+
+                        }
+                    }
+
                 }
             }
         }
--- app/src/main/java/com/example/moviesapp/widgtes/RowMovie.kt
+++ app/src/main/java/com/example/moviesapp/widgtes/RowMovie.kt
@@ -1,30 +1,60 @@
 package com.example.moviesapp.widgtes
 
+import android.graphics.fonts.FontStyle
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.moviesapp.mock.listMovies
 import com.example.moviesapp.models.MoviesModel
 
 @Composable
 fun RowMovies(movie: MoviesModel, handleClickCard: (String) -> Unit) {
+    //referencia
+    //https://stefma.medium.com/jetpack-compose-remember-mutablestateof-derivedstateof-and-remembersaveable-explained-270dbaa61b8
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     //referencia
     //https://developer.android.com/jetpack/compose/designsystems/material3
     //https://foso.github.io/Jetpack-Compose-Playground/foundation/shape/
@@ -32,7 +62,7 @@ fun RowMovies(movie: MoviesModel, handleClickCard: (String) -> Unit) {
         modifier = Modifier
             .padding(vertical = 7.dp, horizontal = 12.dp)
             .fillMaxWidth()
-            .height(130.dp)
+
             .clickable {
                 handleClickCard(movie.id)
             },
@@ -45,7 +75,6 @@ fun RowMovies(movie: MoviesModel, handleClickCard: (String) -> Unit) {
     ) {
         Row(
             modifier = Modifier
-                .fillMaxHeight()
                 .padding(horizontal = 10.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start,
@@ -58,13 +87,77 @@ fun RowMovies(movie: MoviesModel, handleClickCard: (String) -> Unit) {
                 shape = RoundedCornerShape(10.dp),
                 shadowElevation = 4.dp
             ) {
+                //tem que instalar o coil
+                //https://coil-kt.github.io/coil/compose/
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current).data(movie.images[0])
+                        .crossfade(true).build(),
+                    contentDescription = "Image Poster",
+                    contentScale = ContentScale.FillHeight
+
+                )
+                //default icons
+                //Icon(
+                //    imageVector = Icons.Default.AccountBox,
+                //    contentDescription = "Image icon movie"
+                //  )
+            }
+            Column(modifier = Modifier.padding(horizontal = 10.dp, vertical = 15.dp)) {
+                Text(text = movie.title, style = MaterialTheme.typography.titleMedium)
+                Text(
+                    text = "Director: ${movie.director}",
+                    style = MaterialTheme.typography.labelSmall
+                )
+                Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.labelSmall)
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        //text abaixo e parecido com rico text do flutter
+                        Text(text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 13.sp,
+                                    color = Color.DarkGray,
+                                )
+                            ) {
+                                append("Plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 13.sp,
+                                    color = Color.DarkGray,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                append(movie.plot)
+                            }
+                        })
+
+                        Divider(modifier = Modifier.padding(vertical = 5.dp))
+                        Text(
+                            text = "Actors: ${movie.actors}",
+                            style = MaterialTheme.typography.labelSmall
+                        )
+                        Text(
+                            text = "Rating: ${movie.rating}",
+                            style = MaterialTheme.typography.labelSmall
+                        )
+                    }
+                }
                 Icon(
-                    imageVector = Icons.Default.AccountBox,
-                    contentDescription = "Image icon movie"
+                    modifier = Modifier.clickable {
+                        expanded = !expanded
+                    },
+                    imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Icon expanded card"
                 )
             }
-            Text(modifier = Modifier.padding(horizontal = 10.dp), text = movie.title)
         }
     }
+}
 
+
+@Preview
+@Composable
+fun RowMoviesPreview() {
+    RowMovies(movie = listMovies[0], handleClickCard = { })
 }
